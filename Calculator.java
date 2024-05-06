84a17a89186da5c8e9910fd802dcf087ce1e0e66
Fiorela <clafiorelariza@gmail.com>
2024-05-06 10:11:53 -0500

    arreglando el formato

--- Calculator.java
+++ Calculator.java
@@ -3,30 +3,33 @@
 
 public class Calculator {
 
+	public double sumar(int a, int b) {
+		double suma = a + b;
+		return suma;
+	}
 
-public double sumar(int a, int b) {
-    double	suma=a+b;
-	return suma;
-}
-public double restar(int a, int b) {
-    double	resta=a-b;
-	return resta;
-}
-public double multiplciar(int a, int b) {
-    double producto=a*b;
-	return producto;
-}
-public double dividir(int a, int b) {
-    double	cociente=a/b;
-	return cociente;
-}
+	public double restar(int a, int b) {
+		double resta = a - b;
+		return resta;
+	}
 
-public int mod(int a, int b){ 
-	int modulo = a%b;
-	return modulo;
-}
-public double raiz(int a) {
-    
-	return Math.sqrt(a);
-}
+	public double multiplciar(int a, int b) {
+		double producto = a * b;
+		return producto;
+	}
+
+	public double dividir(int a, int b) {
+		double cociente = a / b;
+		return cociente;
+	}
+
+	public int mod(int a, int b) {
+		int modulo = a % b;
+		return modulo;
+	}
+
+	public double raiz(int a) {
+
+		return Math.sqrt(a);
+	}
 }
