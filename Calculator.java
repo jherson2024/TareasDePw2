fd536b953368530de1fc09270a111cea7210c43a
Jherson <you@example.com>
2024-05-06 09:22:42 -0500

    primer commit

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+public double sumar(int a, int b) {
+    double	suma=a+b;
+	return suma;
+}
+public double restar(int a, int b) {
+    double	resta=a-b;
+	return resta;
+}
+public double multiplciar(int a, int b) {
+    double producto=a*b;
+	return producto;
+}
+public double dividir(int a, int b) {
+    double	cociente=a/b;
+	return cociente;
+}
+
+}
